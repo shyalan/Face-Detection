81ce5bf0c3e0259af5abeed558d024f2967cb65f
Shyalan <rshyalan@gmail.com>
2023-12-04 20:12:00 +1100

    Main

---
+++ face_detection.py
@@ -0,0 +1,36 @@
+import cv2
+
+def detect_faces_webcam():
+    # Load the pre-trained Haar Cascade classifier for face detection
+    face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+
+    # Open a connection to the webcam (usually 0 or 1, depending on your setup)
+    cap = cv2.VideoCapture(0)
+
+    while True:
+        # Read a frame from the webcam
+        ret, frame = cap.read()
+
+        # Convert the frame to grayscale
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        # Perform face detection
+        faces = face_cascade.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=5)
+
+        # Draw rectangles around the detected faces
+        for (x, y, w, h) in faces:
+            cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
+
+        # Display the result
+        cv2.imshow('Face Detection - Webcam', frame)
+
+        # Break the loop if 'q' is pressed
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    # Release the webcam and close the window
+    cap.release()
+    cv2.destroyAllWindows()
+
+# Call the function to detect faces using the webcam
+detect_faces_webcam()
